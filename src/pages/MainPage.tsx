a9a85f598c81b4a4c80b7f304958e1306b333188
Deryabin Roman <deryabinrs@gmail.com>
2024-12-17 00:33:47 +0700

    add products

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
 import { Col, Grid, Row } from 'antd';
 import Hipocratus from '@/assets/images/hipocratus.png';
+import products from '@/db/products.json';
 
 const { useBreakpoint } = Grid;
 
@@ -36,57 +37,81 @@ const MainPage = () => {
                         <Col
                             xs={{ flex: '100%' }}
                             sm={{ flex: '100%' }}
-                            lg={{ flex: '50%' }}
-                            style={{ 
-                                background: 'rgb(0, 0, 0, 0.4)', 
-                                padding: '20px 30px', 
-                                display: 'flex',
-                                gap: 10,
-                                borderRadius: 10,
-                                boxShadow: '0 0 40px #000',
-                            }}
+                            md={{ flex: '100%' }}
+                            lg={{ flex: '100%' }}
+                            xl={{ flex: '60%' }}
                         >
-                            <div>
-                                <img src={Hipocratus} alt="Hipocratus" width={screens.sm ? 200 : 120} />
-                            </div>
-                            <div 
-                                style={{ 
-                                    color: '#d9d9d9', 
-                                    fontSize: screens.sm ? 25 : 16, 
-                                    fontWeight: 600,
-                                    padding: 10,
-                                    letterSpacing: 2,
-                                    fontFamily: 'cursive',
-                                }}
-                            >
+                            <div style={{ 
+                                padding: '20px 30px', 
+                                margin: 10
+                            }}>
                                 <div>
-                                    Ваша пища должна быть лекарством, а ваше лекарство должно быть пищей.
+                                    <img 
+                                        src={Hipocratus} 
+                                        alt="Hipocratus"
+                                        width={screens.sm ? 400 : 220}
+                                        style={{
+                                            filter: 'drop-shadow(5px 5px 55px #222)',
+                                        }}
+                                    />
                                 </div>
-                                <div style={{ marginTop: 20, textAlign: 'right' }}>
-                                    Гиппократ
+                                <div 
+                                    style={{ 
+                                        color: '#fff', 
+                                        fontSize: screens.sm ? 25 : 16, 
+                                        fontWeight: 500,
+                                        padding: '20px 30px',
+                                        letterSpacing: 4,
+                                        background: 'rgba(0, 0, 0, 0.5)',
+                                        borderRadius: '5px 0 0 5px',
+                                    }}
+                                >
+                                    <div>
+                                        Ваша пища должна быть лекарством, а ваше лекарство должно быть пищей.
+                                    </div>
+                                    <div style={{ marginTop: 20, textAlign: 'right' }}>
+                                        Гиппократ
+                                    </div>
                                 </div>
                             </div>
+                            
                         </Col>
                         <Col
                             xs={{ flex: '100%' }}
                             sm={{ flex: '100%' }}
-                            lg={{ flex: '50%' }}
-                        ></Col>
+                            md={{ flex: '100%' }}
+                            lg={{ flex: '100%' }}
+                            xl={{ flex: '40%' }}
+                        >
+                            <div style={{ height: 30, background: 'linear-gradient(110deg, rgba(55, 65, 81, 0) 0%, rgba(242,150,81,0.8) 45%, rgb(95 181 106 / 55%) 45%, rgb(95 181 106 / 0%) 100%)', marginTop: '150px', }} />
+                            <div style={{
+                                color: 'white',
+                                letterSpacing: 4,
+                            }}>
+                                <h1 style={{ fontSize: screens.sm ? 30 : 16, textAlign: 'end' }}>
+                                    Функциональное питание - это и есть самый естественный способ поддержания здоровья.
+                                </h1>
+                            </div>
+                        </Col>
                     </Row>
                 </div>
             </div>
-            <div style={{ 
-                width: '100%',
-                maxWidth: 1300,
-                margin: '0 auto'
-            }}>
-                <div style={{
-                    background: 'white',
-                    padding: '30px 20px',
-                    marginTop: '-20px',
-                    borderRadius: '5px',
+            <div style={{ background: 'white', position: 'relative' }}>
+                <div style={{ 
+                    width: '100%',
+                    maxWidth: 1300,
+                    margin: '0 auto',
+                    padding: 20,
                 }}>
-                    MainPage
+                    <div className='bg-green-1' style={{marginTop: -60, padding: '20px 30px', color: 'white'}}><h2>КАТАЛОГ</h2></div>
+                    <div className='catalog'>
+                        {products.map(product => (
+                            <div key={product.name} className='card-product'>
+                                <div><img src={`/img/products/${product.img}`} alt="" /></div>
+                                <div>{product.name}</div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
